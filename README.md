14abec92ee160bbef2bf68a4ca84a097b2b7145d
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2019-06-18 22:38:49 +0200

    feat: add echo POST route

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 2. Clone the repository
 3. Install CORS ```$ go get github.com/heppu/simple-cors```
 4. Run the server ```$ go run hello.go```
-5. Open localhost on a browser (or postman) and try both routes ("/" and "/hello")
+5. Open localhost on a browser (or postman) and try the GET routes ("/" and "/hello") and the POST route ("/echo").
--- hello.go
+++ hello.go
@@ -29,6 +29,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	// Route handlers
+	mux.HandleFunc("/echo", serveAndLog(echo))
 	mux.HandleFunc("/hello", serveAndLog(helloWorld))
 	mux.HandleFunc("/", serveAndLog(defaultServe))
 	// Start the server
@@ -48,6 +49,46 @@ func helloWorld(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 	writeJSON(res, JSON{
 		"message": "Hello World",
+	})
+}
+
+// "echo" route server
+func echo(res http.ResponseWriter, req *http.Request) {
+	// Only listen to POST requests
+	if req.Method != http.MethodPost {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		writeJSON(res, JSON{
+			"message": "Only POST requests are allowed on this route.",
+		})
+		return
+	}
+
+	// Make sure the data sent is in a JSON format.
+	data := JSON{}
+	err := json.NewDecoder(req.Body).Decode(&data)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		writeJSON(res, JSON{
+			"message": "I could not understand what you said because it wasn't written in a JSON format!",
+		})
+		return
+	}
+
+	// Colse body when out of scope.
+	defer req.Body.Close()
+
+	// Make sure the data sent is in "data"
+	reqData, received := data["data"]
+	if !received {
+		res.WriteHeader(http.StatusBadRequest)
+		writeJSON(res, JSON{
+			"message": "No data received.",
+		})
+		return
+	}
+	res.WriteHeader(http.StatusOK)
+	writeJSON(res, JSON{
+		"message": reqData,
 	})
 }
 
